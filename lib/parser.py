e7c55f9458dbb4617d69de10d13c17ce57412782
Tai-Lin Wu <tlw@Tai-Lins-MacBook-Pro.local>
2017-11-23 02:11:22 -0500

    Finish basic parsing API

--- lib/parser.py
+++ lib/parser.py
@@ -1,9 +1,9 @@
 """Iteratively parse data into mongoDB
-
 Dependency: ijson
 
 
 """
+import io
 import ijson
 
 
@@ -20,12 +20,30 @@ class Parser(object):
                 counter += 1
                 if counter == num:
                     break
-                yield line
-
+                yield line 
     def print_entries(self, num):
         """print entries"""
         for i in self.get_entries(num):
             print(i)
+
+    def read_thousand_lines(self):
+        """Process 1000 lines a time"""
+        with open(self.file_path, 'rb') as json_file:
+            l = list()
+            counter = 0
+            for line in json_file:
+                counter += 1
+                if counter == 1000:
+                    break
+                l.append(line)
+            return l
+    
+    def ijson_reader(self, list_json):
+        """Parse a list of json files"""
+        for json in list_json:
+            item = ijson.items(io.BytesIO(json), "")
+            for i in item:
+                print(i['business_id'])
 
 
 def test():
@@ -36,5 +54,13 @@ def test():
         print(i)
 
 
+def test_ijson_reader():
+    business = "/Users/tlw/Desktop/yelp-data/test.json"
+    parser = Parser(business)
+    jsons = parser.read_thousand_lines()
+    parser.ijson_reader(jsons)
+
+
 if __name__ == "__main__":
-    test()
+    # test()
+    test_ijson_reader()

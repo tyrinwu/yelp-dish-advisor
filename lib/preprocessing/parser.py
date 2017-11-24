b94b233ecbd3b00207ae3e9df696238368e3a172
Tai-Lin Wu <tlw@Tai-Lins-MacBook-Pro.local>
2017-11-23 22:33:41 -0500

    add docstring to get_entries

--- lib/preprocessing/parser.py
+++ lib/preprocessing/parser.py
@@ -13,7 +13,11 @@ class Parser(object):
         self.file_path = file_path
 
     def read_lines(self, NUM=1000):
-        """Read `num` lines a time and yield it"""
+        """Read `num` lines a time and yield it
+        
+        Args:
+            NUM (int): number of json strings parsed
+        """
         with open(self.file_path, 'rb') as json_file:
             thousand_lines = list()
             counter = 0
@@ -34,11 +38,18 @@ class Parser(object):
                 yield val
 
     def get_entries(self, num):
-        """Get `num` of entries."""
+        """Get `num` of entries.
+
+        Note:
+            Currently only support num as a factor of 1000
+            i.e. num % 1000 = 0
+        
+        TODO:
+            Refactor it to take exactly `num` entries
+        """
         counter = 0
-        for thousand_lines in self.read_lines():
-            print(thousand_lines)
-            for entry in self.iter_parse(thousand_lines):
+        for lines in self.read_lines():
+            for entry in self.iter_parse(lines):
                 yield entry
             counter += 1000
             if counter > num:
@@ -46,7 +57,7 @@ class Parser(object):
 
 
 def test_ijson_reader():
-    """Testing ijson"""          
+    """Testing ijson"""
     business = "../../data/testParser.json"
     parser = Parser(business)
     jsons = parser.read_lines()
@@ -55,12 +66,13 @@ def test_ijson_reader():
 
 
 def test_get_entries(file_path):
+    """"""
     parser = Parser(file_path)
     for i in parser.get_entries(2000):
         pprint(i)
 
 
 if __name__ == "__main__":
-    # To test the program, chancge 
+    # To test the program, chancge the review_path below
     review_path = "/Users/tlw/Desktop/yelp-data/review.json"
     test_get_entries(review_path)

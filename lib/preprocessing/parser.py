6db009357065ffaed3ebb7b936bed6839cceb232
Tai-Lin Wu <tlw@Tai-Lins-MacBook-Pro.local>
2017-11-24 22:56:33 -0500

    allow get_entries to return subfields

--- lib/preprocessing/parser.py
+++ lib/preprocessing/parser.py
@@ -9,8 +9,8 @@ from pprint import pprint
 
 class Parser(object):
     """Iterative Parser
-    
-    Args: 
+
+    Args:
         file_path
 
     Usage:
@@ -21,22 +21,35 @@ class Parser(object):
         self.file_path = file_path
 
     def _read_lines(self):
-        """Private function"""
+        """Return a generator."""
         with open(self.file_path, 'rb') as json_file:
-            thousand_lines = list()
             for line in json_file:
                 yield line
 
-    def get_entries(self, num=10):
-        """Get `num` of entries. """
+    def get_entries(self, num=10, unlimited=False, func=lambda x: x):
+        """Get entries.
+
+        One can assign a fix number num or set unlimited to True to generate
+        as much as possible. Note this function returns an generator.
+
+        Usage:
+            for entry in parser.get_entries():
+                doSomething()
+
+            To get a specific field of the json, add a lambda function to do so.
+
+            for latitude in parser.get_entries(func=lambda x: x['latitude']):
+                doSomethingWithLatitude()
+
+        """
         counter = 0
         for line in self._read_lines():
             counter += 1
             for val in ijson.items(io.BytesIO(line), ""):
-                yield val
-            if counter >= num:
+                yield func(val)
+            if unlimited is False and counter >= num:
                 break
-                
+
 
 def test_ijson_reader():
     """Testing ijson"""
@@ -50,11 +63,11 @@ def test_ijson_reader():
 def test_get_entries(file_path):
     """"""
     parser = Parser(file_path)
-    for i in parser.get_entries(2000):
+    for i in parser.get_entries(2000, func=lambda x: x["latitude"]):
         print(i)
 
 
 if __name__ == "__main__":
     # To test the program, chancge the review_path below
-    review_path = "/Users/tlw/Desktop/yelp-data/review.json"
+    review_path = "/Users/tlw/Desktop/yelp-data/business.json"
     test_get_entries(review_path)
